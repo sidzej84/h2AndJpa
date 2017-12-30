567fd07bf04e66bf54092bf5cb242125750cc5b3
salkidu <salkidu@gmail.com>
2017-12-30 18:38:49 +0100

    added mock to test

--- Zadanie_1/src/test/java/service/TransactionServiceTest.java
+++ Zadanie_1/src/test/java/service/TransactionServiceTest.java
@@ -1,19 +1,49 @@
 package service;
 
+import controller.TransactionController;
 import model.TransactionDetails;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Hashtable;
+
 import static org.assertj.core.api.Assertions.*;
+
+
+@ContextConfiguration(locations = {"classpath:/application-context.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
 
 
 public class TransactionServiceTest {
     public static final String serviceUri = "http://localhost:8080/transactions";
 
+    @Mock
+    private TransactionService transactions;
+
+    @InjectMocks
+    RestTemplate restTemplate;
+    @Autowired
+    private TransactionController transactionController;
+
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void getTransactionShouldReturn200HttpCode() {
         System.out.println("+++++++getTransaction TEST++++++");
-        RestTemplate restTemplate = new RestTemplate();
+
         TransactionDetails transaction = restTemplate.getForObject(serviceUri+"/1",TransactionDetails.class);
         System.out.println(transaction);
     }
@@ -21,21 +51,18 @@ public class TransactionServiceTest {
     @Test
     public void getAllShouldReturn200HttpCode() {
         System.out.println("+++++++getAll TEST++++++");
-        RestTemplate restTemplate = new RestTemplate();
         restTemplate.getForObject(serviceUri+"/", TransactionDetails.class);
     }
 
     @Test
     public void deleteTransactionByIdShouldReturn200HttpCode() {
         System.out.println("+++++++deleteTransactionById TEST++++++");
-        RestTemplate restTemplate = new RestTemplate();
         restTemplate.delete(serviceUri+"/0");
     }
 
     @Test
     public void updateTransactionShouldReturn200HttpCode() {
         System.out.println("+++++++updateTransaction TEST++++++");
-        RestTemplate restTemplate = new RestTemplate();
         TransactionDetails transaction = new TransactionDetails();
         transaction.setCustomerFirstName("Czesław");
         transaction.setCustomerLastName("Niemen");
@@ -45,7 +72,6 @@ public class TransactionServiceTest {
     @Test
     public void saveTransactionShouldReturn201HttpCode() {
         System.out.println("+++++++saveTransaction TEST++++++");
-        RestTemplate restTemplate = new RestTemplate();
         TransactionDetails transaction = new TransactionDetails();
         transaction.setCustomerFirstName("Jan Stefan");
         transaction.setCustomerLastName("Żeromski");
@@ -54,7 +80,7 @@ public class TransactionServiceTest {
     @Test (expected = HttpClientErrorException.class)
     public void transactionControllerShouldThrowNotFoundException() {
         System.out.println("+++++++transactionNotFoundException TEST++++++");
-        RestTemplate restTemplate = new RestTemplate();
+
         TransactionDetails transaction = restTemplate.getForObject(serviceUri+"/22",TransactionDetails.class);
         assertThat(transaction.getId()).isEqualTo(22);
     }
